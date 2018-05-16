1515cca775f1d39a76bbaca701a516a3a5adfa4a
SebastienBLT <37865399+SebastienBLT@users.noreply.github.com>
2018-05-16 18:41:15 +0200

    Update bdd.sql

--- PoD/bdd.sql
+++ PoD/bdd.sql
@@ -48,6 +48,6 @@ CREATE TABLE IF NOT EXISTS virus
     Niveau                          SMALLINT     		        NOT NULL	        DEFAULT 1           COMMENT 'Défini les différents types de virus du plus gentil au plus agressif',
     CONSTRAINT 			    pk_ID_Virus		                PRIMARY KEY (ID_Virus),
     CONSTRAINT 			    fk_ID_Joueurs_Virus  	        FOREIGN KEY(ID_Joueurs) 	        REFERENCES joueurs(ID_Joueurs),
-    CONSTRAINT 			    fk_ID_Ordinateurs               FOREIGN KEY(ID_Ordinateurs) 	    REFERENCES ordinateurs(ID_Ordinateurs)
+    CONSTRAINT 			    fk_ID_Ordinateurs                   FOREIGN KEY(ID_Ordinateurs) 	    REFERENCES ordinateurs(ID_Ordinateurs)
 );
 
