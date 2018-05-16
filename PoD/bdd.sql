0d0e9a4b0983e0818460e63b977d651ab258ee2d
SebastienBLT <37865399+SebastienBLT@users.noreply.github.com>
2018-05-16 18:44:05 +0200

    Update bdd.sql

--- PoD/bdd.sql
+++ PoD/bdd.sql
@@ -45,7 +45,7 @@ CREATE TABLE IF NOT EXISTS virus
     ID_Ordinateurs 		    INTEGER 		                NOT NULL	                        COMMENT	'Permet d\'identifier la machine infectée',
     ID_Joueurs 			    INTEGER			                NOT NULL                                COMMENT 'Permet d\'identifier le joueur qui a infecté',
     Type_Virus 			    CHAR(3) 		                NOT NULL                                COMMENT 'Permet de nommer le type de virus utilisé',
-    Niveau                          SMALLINT     		        NOT NULL	        DEFAULT 1           COMMENT 'Défini les différents types de virus du plus gentil au plus agressif',
+    Niveau                          SMALLINT     		        NOT NULL    DEFAULT 1           COMMENT 'Défini les différents types de virus du plus gentil au plus agressif',
     CONSTRAINT 			    pk_ID_Virus		                PRIMARY KEY (ID_Virus),
     CONSTRAINT 			    fk_ID_Joueurs_Virus  	        FOREIGN KEY(ID_Joueurs) 	        REFERENCES joueurs(ID_Joueurs),
     CONSTRAINT 			    fk_ID_Ordinateurs                   FOREIGN KEY(ID_Ordinateurs) 	    REFERENCES ordinateurs(ID_Ordinateurs)
