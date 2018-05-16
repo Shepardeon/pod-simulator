d730cb5b733da5528ab61fae991415c4aab37bc9
SebastienBLT <37865399+SebastienBLT@users.noreply.github.com>
2018-05-16 17:55:24 +0200

    Update bdd.sql

--- PoD/bdd.sql
+++ PoD/bdd.sql
@@ -35,7 +35,7 @@ CREATE TABLE IF NOT EXISTS ordinateurs
     Disque_Dur			    INTEGER			            NOT NULL	DEFAULT 1           COMMENT 'Niveau de disque dur de la machine du joueur',
     LOG                         TEXT 			            NOT NULL	                    COMMENT 'Fichier texte qui contient les diverses attaques sur la machine',
     CONSTRAINT 			    pk_ID_Ordinateurs	        PRIMARY KEY(ID_Ordinateurs),
-    CONSTRAINT 			    fk_ID_Joueurs 		        FOREIGN KEY (ID_Joueurs) 	    REFERENCES joueurs (ID_Joueurs)
+    CONSTRAINT 			    fk_ID_Joueurs           FOREIGN KEY (ID_Joueurs) 	    REFERENCES joueurs (ID_Joueurs)
 );
 
 
