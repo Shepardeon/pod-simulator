c315d00ee631fed95efc64106dbf921c00fba399
SebastienBLT <37865399+SebastienBLT@users.noreply.github.com>
2018-05-16 18:30:24 +0200

    Update bdd.sql

--- PoD/bdd.sql
+++ PoD/bdd.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS joueurs
     ID_Joueurs                      INTEGER     AUTO_INCREMENT 	        NOT NULL                            COMMENT 'Permet de générer une id pour chaque nouvel utilisateur sur le jeu',
     Pseudo 		            CHAR(20) 		                NOT NULL 		            COMMENT	'Le nouveau joueur se définit un Pseudo qui permet la reconnexion cette valeur est unique',
     Pass                            VARCHAR(70)                         NOT NULL                            COMMENT 'Le mot de passe du joueur',
-    Mail                            CHAR(50)                            NOT NULL            	COMMENT 'Permet d\'éditer le profil unique et permet la reconnexion',
+    Mail                            CHAR(50)                            NOT NULL            	    COMMENT 'Permet d\'éditer le profil unique et permet la reconnexion',
     Valide 		            BOOLEAN 		                NOT NULL	DEFAULT FALSE       COMMENT 'Permet de savoir si l\'utilisateur à validé son compte',
     Chaine_Validation               CHAR(10)                            NOT NULL		            COMMENT 'Coorespond à la chaine envoyé par mail pour finaliser la création du compte',
     Fonds 		            SMALLINT 		                NOT NULL	DEFAULT 0           COMMENT 'Ressources du joueur issue de ses différentes attaques, peut être dilapidée par des joueurs adverses',
