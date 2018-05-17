c9add1ae9543cdc3fa8952926a4c90ea9a4d3a54
SebastienBLT <37865399+SebastienBLT@users.noreply.github.com>
2018-05-17 10:46:04 +0200

    Add files via upload

--- PoD/connexion.php
+++ PoD/connexion.php
@@ -1,1 +1,49 @@
 <?php
+
+
+
+
+
+
+?>
+<head>
+    <title>Interface de connexion</title>
+    <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Import de Bootstrap sur la page -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+
+<body>
+
+
+
+<form name="Connexion" id="Connexion" method="POST" action="connexion.php">
+
+    <fieldset><legend>Connexion</legend>
+        <label>Pseudo :</label><input name="Pseudo" type="text" placeholder="Saisie d'un pseudo" value="<?php if(isset($_SESSION['connexion_pseudo'])) echo $_SESSION['connexion_pseudo']; ?>">
+        <label>Password :</label><input name="Pass" type="password" placeholder="Saisie du Mot de passe">
+        <input type="checkbox" name="cookie" id="cookie"/><label>Se Souvenir de Moi</label>                    
+
+
+
+        <input type="submit" value="Connexion">
+    </fieldset>
+
+</form>    
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+
+
+
+
+</body>
--- PoD/inscription.php
+++ PoD/inscription.php
@@ -1,5 +1,38 @@
 <?php
 
-include_once("libs/libJoueur.php");
 
-inscrireJoueur("Test", "123", "monmail@lel.fr");
+
+?>
+
+<head>
+    <title>Interface d'inscription</title>
+    <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Import de Bootstrap sur la page -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body>
+
+<form name="inscription" id="inscription" method="POST" action="inscription.php">
+
+    <fieldset>
+
+        <label>Mail</label><input name="mail" type="email" >
+        <label>Pseudo</label><input name="Pseudo" type="text" >
+        <label>Passe</label><input name="Passe" type="password">
+        <input name="Inscription" type="submit" value="Inscription">                
+    </fieldset>
+
+</form>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+</body>
+
+
